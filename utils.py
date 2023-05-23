d6e266aea9376922de300dc5d4200e7fadbd829a
Amandus Krantz <krantz.amandus@protonmail.com>
2023-05-23 14:04:29 +0200

    moved namedtuples

---
+++ utils.py
@@ -0,0 +1,6 @@
+from collections import namedtuple
+
+FlightZone = namedtuple("FlightZone", "x y z floor_offset")
+
+DronePosition = namedtuple("DronePosition", "x y z yaw")
+DroneVelocity = namedtuple("DroneVelocity", "dx dy dz")
