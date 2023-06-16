18741b28d0487181b7f4363c9d2119ff90c2e53d
Amandus Krantz <krantz.amandus@protonmail.com>
2023-06-16 16:15:36 +0200

    added interaction between swarmBoid and flower

--- entities/boids/swarmBoid.py
+++ entities/boids/swarmBoid.py
@@ -24,21 +24,24 @@ class SwarmBoid(Boid):
     def __init__(self,
                  uid: str,
                  flight_zone: any,
+                 polination_rate: int,
                  home: any) -> None:
         super().__init__(flight_zone=flight_zone,
                          uid=uid,
                          separation=1)
+        self.polination_rate = polination_rate
+
         self.alignment = 1
         self.cohesion = 1
         self.visual_range = 0.5
-
-        self.polination_rate = 1
         self.sensory_range = 1.0
+
+        self.polination_range = 0.2
 
         self.home = home
         self.at_home = True
 
-        self.detected_inactive_flowers = []
+        self.detected_open_flowers = []
         self.detected_swarm_boids = []
 
         self._state = self._States.ROAMING
@@ -48,34 +51,31 @@ class SwarmBoid(Boid):
     def state(self) -> any:
         return self._state
 
-    def deposit_pollen(self, flower: any) -> None:
-        # is called when we are within a certain distance of a flower
-        pass
+    def pollinate(self, flower: any) -> None:
+        flower.pollen_level += self.polination_rate
 
     def perceive(self, boids: list, vegetation: list) -> None:
         super().perceive(boids, vegetation)
 
-        self.detected_harvester_boids = self.get_entities_of_type(self.detected_boids,
-                                                                  self.type)
+        self.detected_swarm_boids = self.get_entities_of_type(
+            self.detected_boids, self.type)
 
-        detected_flowers = self.get_entities_of_type(self.detected_vegetation,
-                                                     VegetationTypes.FLOWER)
-        self.detected_inactive_flowers = list(
-            filter(lambda f: not f.active, detected_flowers))
+        detected_flowers = self.get_entities_of_type(
+            self.detected_vegetation, VegetationTypes.FLOWER)
+
+        self.detected_open_flowers = sorted(
+            filter(lambda f: f.open, detected_flowers),
+            key=lambda f: self.distance_to_point(f.position))
 
     def update(self, delta_time: float) -> None:
-        if self.detected_swarm_boids:
-            fly_towards_center(self, self.detected_swarm_boids, delta_time)
-            match_velocity(self, self.detected_swarm_boids, delta_time)
+        fly_towards_center(self, self.detected_swarm_boids, delta_time)
+        match_velocity(self, self.detected_swarm_boids, delta_time)
 
-        if self.detected_inactive_flowers:
-            # find the closest inactive flower and move towards that
-            distance_to_flowers = self.distance_to_entities(
-                self.detected_inactive_flowers)
+        if self.detected_open_flowers:
+            move_towards_point(
+                self, self.detected_open_flowers[0].position, 1)
 
-            closest_flower = min(distance_to_flowers,
-                                 key=distance_to_flowers.get())
-
-            # if we are close enough to the flower, start pollinating it
+            if self.distance_to_point(self.detected_open_flowers[0]) < self.pollination_range:
+                self.polinate(self.detected_open_flowers[0])
 
         super().update(delta_time)
--- entities/vegetation/flower.py
+++ entities/vegetation/flower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from entities.vegetation.vegetation import Vegetation
+from enum import Enum, auto, unique
+
+from entities.vegetation.vegetation import Vegetation, VegetationTypes
 
 __author__ = "Amandus Krantz"
 __credits__ = ["Rachael Garrett", "Joseph La Delfa"]
@@ -11,16 +13,96 @@ __status__ = "Prototype"
 
 
 class Flower(Vegetation):
+    @unique
+    class _States(Enum):
+        ACTIVE: int = auto()
+        SYNTHESISING: int = auto()
+        INACTIVE: int = auto()
+
     def __init__(self,
                  uid: str,
                  position: any,
                  collision_radius: float,
                  activation_radius: float,
-                 polination_threshold: int) -> None:
-        super().__init__(uid,
-                         position,
-                         collision_radius,
-                         activation_radius)
+                 pollen_capacity: int,
+                 pollen_threshold: int,
+                 energy_threshold: int) -> None:
+        super().__init__(uid=uid,
+                         position=position,
+                         collision_radius=collision_radius,
+                         activation_radius=activation_radius)
 
-        self.polination_threshold = polination_threshold
-        self.polination_level = 0.0
+        self._pollen_capacity = pollen_capacity
+        self._pollen_threshold = pollen_threshold
+        self._energy_threshold = energy_threshold
+
+        self.pollen_level = 0.0
+        self.energy_level = 0.0
+
+        self._synthesis_rate = 1.0
+
+        self._type = VegetationTypes.FLOWER
+
+        self._state = self._States.INACTIVE
+
+        self._open = True
+
+   @property
+   def open(self) -> bool:
+       return self._open
+
+   @property
+   def state(self) -> any:
+       return self._state
+
+    def update(self) -> None:
+        """
+        The Flower moves between different states depending on its pollen and
+        energy levels.
+
+        The Flower only accepts new pollen at the start of the
+        inactive->synthesis->active cycle.
+        """
+
+        match self._state:
+            case self._States.INACTIVE:
+                """
+                In the inactive state, the SwarmBoids will deposit "pollen" in the Flower.
+
+                Once there is enough pollen, the Flower will move to the synthesis state.
+                """
+
+                if self.pollen_level < self.pollen_capacity:
+                    self._open = True
+                else:
+                    self._open = False
+                    self._state = self._States.SYNTHESISING
+
+            case self._States.SYNTHESISING:
+                """
+                In the synthesis state, the Flower converts available pollen to energy.
+
+                When there is enough energy, the Flower moves to the active state.
+                """
+
+                # TODO: These should be tuned
+                self.energy_level += self._synthesis_rate
+                self.pollen_level -= self._synthesis_rate
+
+                if self.energy_level > self._energy_threshold:
+                    self._state = self._States.ACTIVE
+
+            case self._States.ACTIVE:
+                """
+                In the active state, the HarvesterBoids are attracted to the Flower
+                and starts harvesting the energy.
+
+                Once the energy has been depleted, the Flower moves back to its
+                inactive state.
+                """
+
+                if self.energy_level < self._energy_threshold:
+                    self._state = self._States.INACTIVE
+
+            case _:
+                raise ValueError(f"Flower with id {self._uid} has unknown state {self._state}")
