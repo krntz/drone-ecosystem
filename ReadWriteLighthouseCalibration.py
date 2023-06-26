ac3eb9b91adef15327221a193cb63d67cc2786bd
Amandus Krantz <krantz.amandus@protonmail.com>
2023-06-26 20:37:51 +0200

    started massive rewrite of read-write module

--- ReadWriteLighthouseCalibration.py
+++ ReadWriteLighthouseCalibration.py
@@ -1,28 +1,51 @@
-from cflib.crazyflie.mem import LighthouseBsCalibration
-from cflib.crazyflie.mem import LighthouseBsGeometry
-from cflib.crazyflie.mem import LighthouseMemHelper
-import cflib.crtp
+#!/usr/bin/env python3
 
-from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.crazyflie import Crazyflie
-
+import argparse
+import logging
 from threading import Event
 
+import cflib.crtp
 import yaml
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.mem import (LighthouseBsCalibration, LighthouseBsGeometry,
+                                 LighthouseMemHelper)
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+__author__ = "Amandus Krantz"
+__credits__ = ["Bitcraze AB"]
+__license__ = "GPL-3"
+__maintainer__ = "Amandus Krantz"
+__email__ = "amandus.krantz@lucs.lu.se"
+__status__ = "Prototype"
+
+logger = logging.getLogger(__name__)
+
 
 class WriteMem:
-    def __init__(self, scf, geo_dict, calib_dict):
-        self._event = Event()
+    def __init__(self, geo_data, calib_data):
+        self._write_event = Event()
+
+        self._geo_data = geo_data
+        self._calib_data = calib_data
+
+    def to_drone(self, scf):
+        """
+        Writes geometry and calibration data to a Synchronous Crazyflie instance.
+        """
 
         helper = LighthouseMemHelper(scf.cf)
 
-        helper.write_geos(geo_dict, self._data_written)
-        self._event.wait()
+        logger.debug(
+            f"Writing geometry data to drone with URI {scf.cf.link_uri}")
+        helper.write_geos(self._geo_data, self._data_written)
+        self._write_event.wait()
 
-        self._event.clear()
+        self._write_event.clear()
 
-        helper.write_calibs(calib_dict, self._data_written)
-        self._event.wait()
+        logger.debug(
+            f"Writing calibration data to drone with URI {scf.cf.link_uri}")
+        helper.write_calibs(self._calib_data, self._data_written)
+        self._write_event.wait()
 
     def _data_written(self, success):
         if success:
@@ -30,97 +53,138 @@ class WriteMem:
         else:
             print('Write failed')
 
-        self._event.set()
+        self._write_event.set()
 
-class ReadMem:
-    def __init__(self, uri = "radio://0/80/2M/E7E7E7E7E0"):
+    def to_file(self, filename: str):
+        """
+        Writes geometry and calibration data to a local file.
+        """
 
-        self._event = Event()
-        self.geo_data = None
-        self.calib_data = None
+        geo_dict = {i: geo.as_file_object()
+                    for i, geo in self._geo_data.items()}
 
-        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-            helper = LighthouseMemHelper(scf.cf)
+        calib_dict = {i: calib.as_file_object()
+                      for i, calib in self._calib_data.items()}
 
-            helper.read_all_geos(self._geo_read_ready)
-            self._event.wait()
+        file_dict = {"geos": geo_dict, "calibs": calib_dict}
 
-            self._event.clear()
-
-            helper.read_all_calibs(self._calib_read_ready)
-            self._event.wait()
-
-
-    def _geo_read_ready(self, geo_data):
-        self.geo_data = geo_data
-        self._event.set()
-
-    def _calib_read_ready(self, calib_data):
-        self.calib_data = calib_data
-        self._event.set()
-    
-    def toFile(self, fn):
-        geo_dict = {}
-        
-        for i, geo in self.geo_data.items():
-            geo_dict[i] = geo.as_file_object()
-        
-        calib_dict = {}
-
-        for i, calib in self.calib_data.items():
-            calib_dict[i] = calib.as_file_object()
-
-        file_dict = {"geos" : geo_dict, "calibs": calib_dict}
-
-        with open(fn, 'w') as f:
+        logger.debug(f"Writing geometry and calibration data to {filename}")
+        with open(filename, 'w') as f:
             yaml.dump(file_dict, f)
 
 
-    def getGeoAndCalib(self):
-        return self.geo_data, self.calib_data
+class ReadMem:
+    def __init__(self) -> None:
+        self._read_event = Event()
+        self._geo_data = {}
+        self._calib_data = {}
 
-def ReadFromFile(fn):
-    with open(fn, 'r') as f:
-        lighthouseConfig = yaml.safe_load(f)
-        
-    lhCalibs = lighthouseConfig["calibs"]
-    lhGeos = lighthouseConfig["geos"]
+    @property
+    def geo_data(self):
+        return self._geo_data
 
-    geo_dict = {}
-    for i in range(len(lhGeos)):
-        geo = lhGeos[i]
-        bsGeo = LighthouseBsGeometry()
-        bsGeo.origin = geo["origin"]
-        bsGeo.rotation_matrix = geo["rotation"]
-        bsGeo.valid = True
+    @property
+    def calib_data(self):
+        return self._calib_data
 
-        geo_dict[i] = bsGeo
+    def from_drone(self, scf) -> None:
+        """
+        Reads geometry and calibration data from a Synchronous Crazyflie instance.
+        """
 
-    calib_dict = {}
+        helper = LighthouseMemHelper(scf.cf)
 
-    for i in range(len(lhCalibs)):
-        calib = lhCalibs[i]
-        bsCalib = LighthouseBsCalibration()
-        bsCalib.uid = calib["uid"]
-        bsCalib.valid = True
+        helper.read_all_geos(self._geo_read_ready)
+        self._read_event.wait()
 
-        for j, sweep in enumerate(calib["sweeps"]):
-            bsCalib.sweeps[j].phase = sweep["phase"]
-            bsCalib.sweeps[j].tilt = sweep["tilt"]
-            bsCalib.sweeps[j].curve = sweep["curve"]
-            bsCalib.sweeps[j].gibmag = sweep["gibmag"]
-            bsCalib.sweeps[j].gibphase = sweep["gibphase"]
-            bsCalib.sweeps[j].ogeemag = sweep["ogeemag"]
-            bsCalib.sweeps[j].ogeephase = sweep["ogeephase"]
-        
-        calib_dict[i] = bsCalib
-    
-    return geo_dict, calib_dict
+        self._read_event.clear()
+
+        helper.read_all_calibs(self._calib_read_ready)
+        self._read_event.wait()
+
+    def _geo_read_ready(self, geo_data) -> None:
+        self._geo_data = geo_data
+        self._read_event.set()
+
+    def _calib_read_ready(self, calib_data) -> None:
+        self._calib_data = calib_data
+        self._read_event.set()
+
+    def from_file(self, filename: str) -> None:
+        """
+        Reads geometry and calibration data from a local file.
+        """
+
+        with open(filename, 'r') as f:
+            lighthouseConfig = yaml.safe_load(f)
+
+        lighthouse_calibration = lighthouseConfig["calibs"]
+        lighthouse_geometry = lighthouseConfig["geos"]
+
+        for i in range(len(lighthouse_geometry)):
+            geo = lighthouse_geometry[i]
+            bsGeo = LighthouseBsGeometry()
+            bsGeo.origin = geo["origin"]
+            bsGeo.rotation_matrix = geo["rotation"]
+            bsGeo.valid = True
+
+            self.geo_dict[i] = bsGeo
+
+        for i in range(len(lighthouse_calibration)):
+            calib = lighthouse_calibration[i]
+            bsCalib = LighthouseBsCalibration()
+            bsCalib.uid = calib["uid"]
+            bsCalib.valid = True
+
+            for j, sweep in enumerate(calib["sweeps"]):
+                bsCalib.sweeps[j].phase = sweep["phase"]
+                bsCalib.sweeps[j].tilt = sweep["tilt"]
+                bsCalib.sweeps[j].curve = sweep["curve"]
+                bsCalib.sweeps[j].gibmag = sweep["gibmag"]
+                bsCalib.sweeps[j].gibphase = sweep["gibphase"]
+                bsCalib.sweeps[j].ogeemag = sweep["ogeemag"]
+                bsCalib.sweeps[j].ogeephase = sweep["ogeephase"]
+
+            self.calib_dict[i] = bsCalib
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.ERROR)
+
+    parser = argparse.ArgumentParser(
+        description="Reads and/or writes calibration data from/to Crazyflie drones")
+
+    parser.add_argument('-i', '--input', required=True, dest='input',
+                        help='Input to read calibration data from, can be either ' +
+                        'a local file or the URI of an already calibrated drone')
+
+    parser.add_argument('-o', '--output', required=True, dest='output',
+                        help='Output to write calibration data to, can be either a file or a drone')
+
+    args = parser.parse_args()
+
+    cflib.crtp.init_drivers()
+
+    read_mem = ReadMem()
+
+    input_is_drone = args.input.startswith("radio://")
+
+    if input_is_drone:
+        with SyncCrazyflie(args.input, cf=Crazyflie(rw_cache='./cache')) as scf:
+            read_mem.from_drone(scf)
+    else:
+        read_mem.from_file(args.input)
+
+    write_mem = WriteMem(read_mem.geo_data, read_mem.calib_data)
+
+    output_is_drone = args.output.startswith("radio://")
+
+    if output_is_drone:
+        with SyncCrazyflie(args.output, cf=Crazyflie(rw_cache='./cache')) as scf:
+            write_mem.to_drone(scf)
+    else:
+        write_mem.to_file(args.output)
 
 
 if __name__ == "__main__":
-    # TODO: Sort out parameters etc. to allow choosing to read and/or write to/from a drone or file
-
-    cflib.crtp.init_drivers()
-    mem = ReadMem()
-    mem.toFile("config.yaml")
+    main()
