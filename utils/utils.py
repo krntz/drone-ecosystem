f30628bdc204bf0b17f74dc2cf4bce2619cda298
Amandus Krantz <krantz.amandus@protonmail.com>
2024-05-14 09:44:30 +0200

    moved controllers to new repo

--- utils/utils.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-
-FlightZone = namedtuple("FlightZone", "x y z floor_offset")
-
-DronePosition = namedtuple("DronePosition", "x y z yaw")
-DroneVelocity = namedtuple("DroneVelocity", "vx vy vz yawrate")
